5a6d3c1f4b758a6ccff9276ac15be17ea16b1fa8
Praxthisnovcht <praxthisnovcht@gmail.com>
2015-05-01 14:00:34 +0200

    Forgot upload

--- KillCash/src/Praxthisnovcht/KillCash/Main.php
+++ KillCash/src/Praxthisnovcht/KillCash/Main.php
@@ -22,15 +22,17 @@ class Main extends PluginBase implements Listener{
 	public function onEnable(){
 		 @mkdir($this->getDataFolder());
 
-		     $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML, array(
-			     "money"  => 100,
-			         "message" => "You kill {name} and earn {money} $"
+      $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML, array(
+        "money-Drop"  => 100,
+        "money-Lost"  => 100,
+            "message-Drop" => "You kill {Name} and earn {Money} coins",
+            "message-Lost" => "you was killed by {Name} and lost {Money} coins"
                          ));
         
                              $this->getServer()->getPluginManager()->registerEvents($this, $this);
                                  $load = $this->getServer()->getPluginManager();
                                       if(!($this->money = $load->getPlugin("PocketMoney"))  && !($this->money = $load->getPlugin("EconomyAPI")) && !($this->money = $load->getPlugin("MassiveEconomy"))){
-                                         $this->getLogger()->info(TextFormat::RED."KillCash not loaded, I can't find Plugin Economy");
+                                         $this->getLogger()->info(TextFormat::GOLD." .KillCash not loaded, I can't find Plugin Economy");
                                              $this->getLogger()->info(TextFormat::GOLD.". Please install:");
                                                  $this->getLogger()->info(TextFormat::GOLD.". MassiveEconomy-EconomyAPI-PocketMoney-EconomyMaster:");
                                                      } else {
@@ -46,10 +48,17 @@ class Main extends PluginBase implements Listener{
         	     $cause = $entity->getLastDamageCause();
 			         if($cause instanceof EntityDamageByEntityEvent) {
 			           	 $killer = $cause->getDamager()->getPlayer();
-						        $message = str_replace("{name}", $entity->getPlayer()->getName(), $this->config->get("message"));
-					                 $message = str_replace("{money}", $this->config->get("money"), $this->config->get("message"));
-					                     $killer->sendMessage("$message");
-					                         $this->grantMoney($killer->getName(), $this->config->get("money"));
+									     # CustomMessage
+										 $Kmessage = str_replace("{Name}", $entity->getPlayer()->getName(), $this->config->get("message-Drop"));
+					                     $Kmessage = str_replace("{Money}", $this->config->get("money-Drop"), $this->config->get("message-Drop"));
+										 $killer->sendMessage("$Kmessage");
+																				 
+										 $Dmessage = str_replace("{Name}", $killer->getPlayer()->getName(), $this->config->get("message-Lost"));
+					                     $Dmessage = str_replace("{Money}", $this->config->get("money-Lost"), $this->config->get("message-Lost"));										 
+										 $entity>sendMessage("$Dmessage");										 										 
+										     # Conclusion
+											$this->reduceMoney($entity->getName(), $this->config->get("money-Lost"));
+					                         $this->grantMoney($killer->getName(), $this->config->get("money-Drop"));
 					                             return true;
 				                                 
 			                                         }else{
@@ -57,6 +66,20 @@ class Main extends PluginBase implements Listener{
 			                                                 }
 		                                                         
 	}
+##########################################################################################################################################	
+ # V_1.0.1
+  public function reduceMoney($p,$money) {
+     if(!$this->money) return false;
+	    switch($this->money->getName()){
+                                     case "EconomyAPI":
+                                         $this->money->reduceMoney($p,$this->money->getMoney($p)-$money);
+                                             break;												 										
+                                                     default:
+                                                         return false;
+                                                             }
+                                                                 return true;
+                                                                     }
+##############################################################################################################################################
   public function grantMoney($p,$money) {
      if(!$this->money) return false;
         switch($this->money->getName()){
