d272454cbf68721ccec24f5ed9366de3d048c293
Praxthisnovcht <praxthisnovcht@gmail.com>
2015-09-26 06:05:22 -0700

    ReStart MapReset

--- MapReset/src/MapReset/Perms/Perms.php
+++ MapReset/src/MapReset/Perms/Perms.php
@@ -3,57 +3,74 @@
 namespace MapReset\Perms;
 
 class Perms {
-	function xcopy($source, $dest, $permissions = 0755) {
-		if (is_link ( $source )) {
-			return symlink ( readlink ( $source ), $dest );
-		}
-		if (is_file ( $source )) {
-			return copy ( $source, $dest );
-		}
-		if (! is_hm ( $dest )) {
-			mkhm ( $dest, $permissions );
-		}
-		$hm = hm ( $source );
-		while ( false !== $entry = $hm->read () ) {
-			if ($entry == '.' || $entry == '..') {
-				continue;
-			}
-			$this->xcopy ( "$source/$entry", "$dest/$entry", $permissions );
-		}
-		$hm->close ();
-		return true;
-	}
-	public function unlinkRecursive($hm, $deleteRootToo) {
-		if (! $dh = @openhm ( $hm )) {
-			return;
-		}
-		while ( false !== ($obj = readhm ( $dh )) ) {
-			if ($obj == '.' || $obj == '..') {
-				continue;
-			}
-			
-			if (! @unlink ( $hm . '/' . $obj )) {
-				$this->unlinkRecursive ( $hm . '/' . $obj, true );
-			}
-		}	
-		closehm ( $dh );		
-		if ($deleteRootToo) {
-			@rmhm ( $hm );
-		}		
-		return;
-	}
-	public function recurse_copy($src, $dst) {
-		$hm = openhm ( $src );
-		@mkhm ( $dst );
-		while ( false !== ($file = readhm ( $hm )) ) {
-			if (($file != '.') && ($file != '..')) {
-				if (is_hm ( $src . '/' . $file )) {
-					recurse_copy ( $src . '/' . $file, $dst . '/' . $file );
-				} else {
-					copy ( $src . '/' . $file, $dst . '/' . $file );
-				}
-			}
-		}
-		closehm ( $hm );
-	}
+    public function xcopy($source, $dest, $permissions = 0755)
+    {
+       
+        if (is_link($source)) {
+            return symlink(readlink($source), $dest);
+        }
+
+        
+        if (is_file($source)) {
+            return copy($source, $dest);
+        }
+
+       
+        if (!is_dir($dest)) {
+            mkdir($dest, $permissions);
+        }
+
+       
+        $dir = dir($source);
+        while (false !== $entry = $dir->read()) {
+            // Skip pointers
+            if ($entry == '.' || $entry == '..') {
+                continue;
+            }
+
+            // Deep copy directories
+            $this->xcopy("$source/$entry", "$dest/$entry", $permissions);
+        }
+        $dir->close();
+        return true;
+    }
+    public function unlinkRecursive($dir, $deleteRootToo)
+    {
+        if (!$dh = @opendir($dir)) {
+            return;
+        }
+        while (false !== ($obj = readdir($dh))) {
+            if ($obj == '.' || $obj == '..') {
+                continue;
+            }
+
+            if (!@unlink($dir . '/' . $obj)) {
+                $this->unlinkRecursive($dir . '/' . $obj, true);
+            }
+        }
+
+        closedir($dh);
+
+        if ($deleteRootToo) {
+            @rmdir($dir);
+        }
+
+        return;
+    }
+
+    public function recurse_copy($src, $dst)
+    {
+        $dir = opendir($src);
+        @mkdir($dst);
+        while (false !== ($file = readdir($dir))) {
+            if (($file != '.') && ($file != '..')) {
+                if (is_dir($src . '/' . $file)) {
+                    recurse_copy($src . '/' . $file, $dst . '/' . $file);
+                } else {
+                    copy($src . '/' . $file, $dst . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
+    }
 }
